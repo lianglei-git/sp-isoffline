e3b09574748d846e0b862ada06a9796720504358
lianglei-git <2838370086@qq.com>
2021-10-14 11:42:23 +0800

    add test code

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const {isonline} = require('./lib/isonline')
 const {offlineListener} = require('./lib/offlinelistener')
 
-offlineListener({
-    time: 1000,
-    // interval: 9,
-    offline(callback) {
-        console.log('网络断开')
-        callback(({ status }) => {
-            if (status) console.log('联网成功')
-            else console.log('网络连接失败')
-        })
-    },
-    online({ status }) {
-        if (status) console.log('网络正常')
-    }
-})
+// offlineListener({
+//     time: 1000,
+//     // interval: 9,
+//     offline(callback) {
+//         console.log('网络断开')
+//         callback(({ status }) => {
+//             if (status) console.log('联网成功')
+//             else console.log('网络连接失败')
+//         })
+//     },
+//     online({ status }) {
+//         if (status) console.log('网络正常')
+//     }
+// })
 
 
 module.exports = {
---
+++ test.js
@@ -0,0 +1,15 @@
+const {offlineListener} = require('./index')
+offlineListener({
+    time: 1000,
+    // interval: 9,
+    offline(callback) {
+        console.log('网络断开')
+        callback(({ status }) => {
+            if (status) console.log('联网成功')
+            else console.log('网络连接失败')
+        })
+    },
+    online({ status }) {
+        if (status) console.log('网络正常')
+    }
+})
