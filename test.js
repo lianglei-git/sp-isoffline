aca1bc69346ff851ea4a665d8e379341bd4499cf
lianglei-git <2838370086@qq.com>
2021-11-04 18:45:50 +0800

    add clear

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const {offlineListener} = require('./index')
-offlineListener({
+let time = offlineListener({
     time: 1000,
-    // interval: 9,
+    // interval: 3,
     offline(callback) {
         console.log('网络断开')
         callback(({ status }) => {
@@ -9,7 +9,12 @@ offlineListener({
             else console.log('网络连接失败')
         })
     },
-    online({ status }) {
-        if (status) console.log('网络正常')
-    }
+    // online({ status }) {
+    //     if (status) console.log('网络正常')
+    // }
 })
+
+setTimeout(() => {
+    console.log(time.clear);
+    time.clear()
+}, 20000)
